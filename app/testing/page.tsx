a5079595ec2656aa751f49565c6aa1475f520483
Andrew <andrew.gary91@gmail.com>
2024-02-07 00:57:15 -0600

    fleshing out some of the folder structure, still getting used to the app router

---
+++ app/testing/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <button className='border border-black'>
+        button
+      </button>
+    </div>
+  ) 
+}
+
+export default page
